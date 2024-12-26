d0024451cff1cd08401caa2ddc2a58218288246d
linelie00 <san00@cu.ac.kr>
2024-12-26 18:02:33 +0900

    [Design]:폰트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './Styles/App.css';
+import './Assets/Font/Font.css';
 import Register from './Pages/Register';
 import Task from './Pages/Task';
 import Schedule from './Pages/Schedule';
