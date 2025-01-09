4f26bf2f69e58f14f03f0c6698af2baa63710812
linelie00 <san00@cu.ac.kr>
2025-01-09 17:28:04 +0900

    [Feat]:시퀄라이즈 연결 및 설정 변경

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 // 필요한 모듈 가져오기
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { sequelize } = require('./models');
 
 // Express 앱 생성
 const app = express();
 
 // 포트 설정
 const PORT = 8080;
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error('데이터베이스 연결 실패:', err);
+  });
+
+  app.use(morgan('dev'));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
 // 기본 라우트 정의
 app.get('/', (req, res) => {
