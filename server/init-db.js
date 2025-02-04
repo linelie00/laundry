9622602cafbb8dd9f22ab88a35c8b8afa72e52e7
linelie00 <san00@cu.ac.kr>
2025-02-04 16:46:17 +0900

    [Feat]:init-db 수정

--- server/init-db.js
+++ server/init-db.js
@@ -2,7 +2,7 @@ const { sequelize } = require('./models');
 
 (async () => {
   try {
-    await sequelize.sync({ force: true }); // 기존 테이블 유지하며 동기화
+    await sequelize.sync({ force: false }); // 기존 테이블 유지하며 동기화
     console.log('테이블이 성공적으로 생성되었습니다.');
   } catch (error) {
     console.error('테이블 생성 중 오류 발생:', error);
